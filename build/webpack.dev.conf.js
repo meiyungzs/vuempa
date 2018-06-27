77d4b4f97e16216c148c9fd436d887e2a9013328
meiyungzs <meiyungzs@gmail.com>
2018-06-27 15:36:32 +0800

    增加ico

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -13,6 +13,7 @@ const portfinder = require('portfinder')
 const HOST = process.env.HOST
 const PORT = process.env.PORT && Number(process.env.PORT)
 
+
 const devWebpackConfig = merge(baseWebpackConfig, {
   module: {
     rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap, usePostCSS: true })
@@ -55,7 +56,8 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: 'index.html',
-      inject: true
+      inject: true,
+      favicon: path.resolve('favicon.ico')//新增favicon.ico
     }),
     // copy custom static assets
     new CopyWebpackPlugin([
